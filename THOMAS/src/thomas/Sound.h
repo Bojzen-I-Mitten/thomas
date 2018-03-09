76e8a2af0d29ae278868358a9f864058fca7090f
unknown <oskar.warnhag@gmail.com>
2018-03-09 17:33:17 +0100

    Added a map for storing individual wave files

--- THOMAS/src/thomas/Sound.h
+++ THOMAS/src/thomas/Sound.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include "DirectXTK/SoundCommon.h"
+#include <map>
 
 
 namespace thomas
@@ -13,12 +14,13 @@ namespace thomas
 	public:
 		static bool Init();
 		static bool Play(std::string name, float volume);
-		static std::unique_ptr<DirectX::SoundEffectInstance> CreateInstance(std::string clipName);
+		static std::unique_ptr<DirectX::SoundEffectInstance> CreateInstance(std::string clipName, DirectX::SOUND_EFFECT_INSTANCE_FLAGS flags);
 		static void SetMasterVolume(float volume);
 		static void SetFxVolume(float volume);
 		static void SetMusicVolume(float volume);
 		static float GetMusicVolume();
 		static bool LoadWaveBank(std::string name);
+		static bool LoadWave(std::string name);
 		static void Destroy();
 		static void Update();
 	private:
@@ -27,5 +29,6 @@ namespace thomas
 		static float s_musicVolume;
 		static std::unique_ptr<DirectX::WaveBank> s_bank;
 		static std::unique_ptr<DirectX::AudioEngine> s_audioEngine;
+		static std::map<std::string, std::unique_ptr<DirectX::SoundEffect>> s_waves;
 	};
 }
