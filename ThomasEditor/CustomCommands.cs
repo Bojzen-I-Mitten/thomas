cbb3481071a9be332cea5ab63eca6b29e4018653
Albin Sahlbom <albinsahlbom@hotmail.com>
2018-03-10 20:16:44 +0100

    Working on Ctrl+A for adding new Component

--- ThomasEditor/CustomCommands.cs
+++ ThomasEditor/CustomCommands.cs
@@ -13,13 +13,16 @@ namespace ThomasEditor.Commands
         private static Key addNewEmptyObject = Key.N;
         private static Key openOptionsMenu = Key.O;
         private static Key play = Key.F5;
+        private static Key addComponent = Key.A;
 
         public static Key GetOpenOptionsMenuKey() { return openOptionsMenu; }
         public static void SetOpenOptionsMenuKey(Key set) { openOptionsMenu = set; }
-        public static Key GetaddNewEmptyObjectKey() { return addNewEmptyObject; }
-        public static void SetaddNewEmptyObjectKey(Key set) { addNewEmptyObject = set; }
+        public static Key GetAddNewEmptyObjectKey() { return addNewEmptyObject; }
+        public static void SetAddNewEmptyObjectKey(Key set) { addNewEmptyObject = set; }
         public static Key GetPlayKey() { return play; }
         public static void SetPlayKey(Key set) { play = set; }
+        public static Key GetAddComponentKey() { return addComponent; }
+        public static void SetAddComponentKey(Key set) { addComponent = set; }
 
 
 
@@ -30,12 +33,14 @@ namespace ThomasEditor.Commands
         //This is a custom command object.
         public static readonly RoutedUICommand NewEmptyObject = new RoutedUICommand
             (
-                "New Empty Object",   //The text that will show on the item that the command is bound to.
-                "NewEmptyObject",   //Name of the command
+                "New Empty Object",     //The text that will show on the item that the command is bound to.
+                "NewEmptyObject",       //Name of the command
                 typeof(CustomCommands), //Owner of command
                 new InputGestureCollection()
                 {
-                    new KeyGesture(addNewEmptyObject, ModifierKeys.Control)
+                    new KeyGesture(             //Specify what keys need to be pressed.
+                        addNewEmptyObject,      //The key
+                        ModifierKeys.Control)   //Ctrl
                 }
             );
 
@@ -61,8 +66,20 @@ namespace ThomasEditor.Commands
                 }
             );
 
+        public static readonly RoutedUICommand AddComponent = new RoutedUICommand
+            (
+                "Add Component",
+                "AddComponent",
+                typeof(CustomCommands),
+                new InputGestureCollection()
+                {
+                    new KeyGesture(addComponent, ModifierKeys.Control)
+                }
+            );
+
         public static RoutedUICommand GetNewEmptyObject { get { return NewEmptyObject; } }
         public static RoutedUICommand GetOpenOptionsWindow { get { return OpenOptionsWindow; } }
         public static RoutedUICommand GetPlay { get { return Play; } }
+        public static RoutedUICommand GetAddCompenent { get { return AddComponent; } }
     }
 }
