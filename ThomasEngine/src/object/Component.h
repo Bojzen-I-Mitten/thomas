5a3cf0cb4e57f3f0d6eef2c1ecf0296965ddaa3e
Morgan Cromell <morgan.cromell@hotmail.com>
2018-04-06 16:48:10 +0200

    Made some updates to the hierarchy

--- ThomasEngine/src/object/Component.h
+++ ThomasEngine/src/object/Component.h
@@ -41,10 +41,7 @@ namespace ThomasEditor {
 
 	public:
 
-		static operator bool(Component^ component)
-		{
-			return component != nullptr;
-		}
+		
 
 		static System::Reflection::Assembly^ editorAssembly;
 		bool enabled = true;
