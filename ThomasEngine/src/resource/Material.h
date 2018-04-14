dcfc183ff007415bd7bb3a4e7da2de76a5ae7a70
Morgan Cromell <morgan.cromell@hotmail.com>
2018-04-15 00:40:51 +0200

    Updated grid to be more optimized

--- ThomasEngine/src/resource/Material.h
+++ ThomasEngine/src/resource/Material.h
@@ -13,6 +13,8 @@ namespace ThomasEditor
 	[DataContractAttribute]
 	public ref class Material : public Resource
 	{
+	private:
+		Dictionary<String^, System::Object^>^ m_editorProperties = gcnew Dictionary<String^, System::Object^>();
 	internal:
 		Material(thomas::resource::Material* ptr) : Resource("", ptr) {};
 		bool m_loaded = false;
@@ -26,10 +28,12 @@ namespace ThomasEditor
 		Material(Shader^ shader) : Resource("", new thomas::resource::Material((thomas::resource::Shader*)shader->m_nativePtr))
 		{
 			m_loaded = true;
+			UpdateEditorProperties();
 		}
 		Material(Material^ original) : Resource("", new thomas::resource::Material((thomas::resource::Material*)original->m_nativePtr))
 		{
 			m_loaded = true;
+			UpdateEditorProperties();
 		}
 		
 		static property Material^ StandardMaterial
@@ -54,8 +58,7 @@ namespace ThomasEditor
 
 		Vector4 GetVector(String^ name) { return Vector4(((thomas::resource::Material*)m_nativePtr)->GetVector(Utility::ConvertString(name))); }
 		void SetVector(String^ name, Vector4 value) { ((thomas::resource::Material*)m_nativePtr)->SetVector(Utility::ConvertString(name), thomas::math::Vector4(value.x, value.y, value.z, value.w)); }
-
-
+		
 		[DataMemberAttribute(Order=0)]
 		property Shader^ Shader
 		{
@@ -68,51 +71,23 @@ namespace ThomasEditor
 				{
 					m_nativePtr = new thomas::resource::Material(Utility::ConvertString(m_path));
 					((thomas::resource::Material*)m_nativePtr)->SetShader((thomas::resource::Shader*)value->m_nativePtr);
-					
 				}
+				UpdateEditorProperties();
 			}
 		}
+
+
 
 		[DataMemberAttribute(Order = 1)]
 		property Dictionary<String^, System::Object^>^ EditorProperties
 		{
 			Dictionary<String^, System::Object^>^ get() {
-				Dictionary<String^, System::Object^>^ props = gcnew Dictionary<String^, System::Object^>();
-
-				for (auto& prop : ((thomas::resource::Material*)m_nativePtr)->GetEditorProperties())
-				{
-					
-					System::Object^ value;
-					switch (prop.second->GetType())
-					{
-					case thomas::resource::shaderProperty::ShaderProperty::Type::SCALAR_BOOL:
-						
-						break;
-					case thomas::resource::shaderProperty::ShaderProperty::Type::SCALAR_FLOAT:
-						value = ((thomas::resource::Material*)m_nativePtr)->GetFloat(prop.first);
-						break;
-					case thomas::resource::shaderProperty::ShaderProperty::Type::SCALAR_INT:
-						value = ((thomas::resource::Material*)m_nativePtr)->GetInt(prop.first);
-						break;
-					case thomas::resource::shaderProperty::ShaderProperty::Type::VECTOR:
-						value = Vector4(((thomas::resource::Material*)m_nativePtr)->GetVector(prop.first));
-						break;
-					case thomas::resource::shaderProperty::ShaderProperty::Type::COLOR:
-						value = Color(((thomas::resource::Material*)m_nativePtr)->GetColor(prop.first));
-						break;
-					case thomas::resource::shaderProperty::ShaderProperty::Type::MATRIX:
-						//value = Matrix4x4(((thomas::resource::Material*)m_nativePtr)->GetMatrix(prop.first));
-						break;
-					default:
-						break;
-					}
-					props->Add(Utility::ConvertString(prop.first), value);
-				}
-				return props;
+				return m_editorProperties;
 			}
 				
 			void set(Dictionary<String^, System::Object^>^ value)
 			{
+				
 				for each(String^ key in value->Keys)
 				{
 					System::Object^ prop = value[key];
@@ -134,8 +109,46 @@ namespace ThomasEditor
 				}
 				if(m_loaded)
 					ThomasEditor::Resources::SaveResource(this, m_path);
+
+				UpdateEditorProperties();
 			}
 		}
+	private:
+		void UpdateEditorProperties()
+		{
+			m_editorProperties->Clear();
+			for (auto& prop : ((thomas::resource::Material*)m_nativePtr)->GetEditorProperties())
+			{
+
+				System::Object^ value;
+				switch (prop.second->GetType())
+				{
+				case thomas::resource::shaderProperty::ShaderProperty::Type::SCALAR_BOOL:
+
+					break;
+				case thomas::resource::shaderProperty::ShaderProperty::Type::SCALAR_FLOAT:
+					value = ((thomas::resource::Material*)m_nativePtr)->GetFloat(prop.first);
+					break;
+				case thomas::resource::shaderProperty::ShaderProperty::Type::SCALAR_INT:
+					value = ((thomas::resource::Material*)m_nativePtr)->GetInt(prop.first);
+					break;
+				case thomas::resource::shaderProperty::ShaderProperty::Type::VECTOR:
+					value = Vector4(((thomas::resource::Material*)m_nativePtr)->GetVector(prop.first));
+					break;
+				case thomas::resource::shaderProperty::ShaderProperty::Type::COLOR:
+					value = Color(((thomas::resource::Material*)m_nativePtr)->GetColor(prop.first));
+					break;
+				case thomas::resource::shaderProperty::ShaderProperty::Type::MATRIX:
+					value = Matrix4x4(((thomas::resource::Material*)m_nativePtr)->GetMatrix(prop.first));
+					break;
+				default:
+					break;
+				}
+				m_editorProperties->Add(Utility::ConvertString(prop.first), value);
+			}
+		}
+
+		
 
 	internal:
 		[OnDeserializedAttribute]
